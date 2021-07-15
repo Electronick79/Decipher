3718bba7daed3daa50b13acfa1bc0bf98c0faf4e
Electronick79 <63924606+Electronick79@users.noreply.github.com>
2021-07-15 19:35:56 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 import lexer
 import parserr
-import evaluator
-while True:
-    srcCode = input(">>> ")
-    if srcCode == "poopoo":
-        break
-    tokSeq = lexer.tokenize(srcCode)
-    rootNode = parserr.parse(tokSeq)
-    result = evaluator.evaluate(rootNode)
-    print("The result is: ", result)
-print("Now it is time to go poo poo.")
+srcCode = "1 * (2 + 5)"
+tokSeq = lexer.tokenize(srcCode)
+rootNode = parserr.parse(tokSeq)
+parserr.printTree(tokSeq)
 
